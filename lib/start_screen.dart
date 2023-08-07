e959d4e0eb969bcdf18b982d47f7963c440b980e
VANUKURI SIVA MOUNIKER <105932128+MOUNIKER@users.noreply.github.com>
2023-08-07 15:34:19 +0530

    Google fonts has been added

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:quiz_app/quiz.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen(this.startQuiz ,{super.key});
+  const StartScreen(this.startQuiz, {super.key});
 
   final void Function() startQuiz;
 
@@ -17,23 +17,23 @@ class StartScreen extends StatelessWidget {
             width: 300,
             color: const Color.fromARGB(150, 255, 255, 255),
           ),
-          const SizedBox(
-            height: 80,
+          const SizedBox(height: 80),
+          Text(
+            'Learn Flutter the fun way!',
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
           ),
-          const Text(
-            "Learn",
-            style: TextStyle(color: Color.fromARGB(255, 237, 223, 252), fontSize: 30),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-        OutlinedButton.icon(
-          onPressed: startQuiz,
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white
-          ),
-          icon: const Icon(Icons.arrow_right_alt),
-           label: const Text("Start Quiz"))
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
         ],
       ),
     );
